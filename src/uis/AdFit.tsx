da684177d15d293aace13a18ef9964f6a8b8ac3b
hyochan <dooboolab@gmail.com>
2023-09-18 18:24:11 +0900

    fix: Cannot read properties of null (reading appendChild)

--- src/uis/AdFit.tsx
+++ src/uis/AdFit.tsx
@@ -2,36 +2,40 @@ import React, { CSSProperties } from "react";
 import { useEffect } from "react";
 import { useMediaQuery } from "react-responsive";
 
-
 export default function AdFit({
-  className = 'adfit',
+  className = "adfit",
   style,
   unit,
   height,
   width,
 }: {
   className?: string;
-  style?: CSSProperties,
+  style?: CSSProperties;
   unit: string;
   width: number;
   height: number;
 }): JSX.Element {
-
   useEffect(() => {
-    let ins = document.createElement("ins");
-    let scr = document.createElement("script");
-    ins.className = "kakao_ad_area";
-    // @ts-ignore
-    ins.style = "display:none; width:100%;";
-    // @ts-ignore
-    scr.async = "true";
-    scr.type = "text/javascript";
-    scr.src = "//t1.daumcdn.net/kas/static/ba.min.js";
-    ins.setAttribute("data-ad-width", width.toString());
-    ins.setAttribute("data-ad-height", height.toString());
-    ins.setAttribute("data-ad-unit", unit.toString());
-    document.querySelector(`.${className}`).appendChild(ins);
-    document.querySelector(`.${className}`).appendChild(scr);
+    const timeout = setTimeout(() => {
+      let ins = document.createElement("ins");
+      let scr = document.createElement("script");
+      ins.className = "kakao_ad_area";
+      // @ts-ignore
+      ins.style = "display:none; width:100%;";
+      // @ts-ignore
+      scr.async = "true";
+      scr.type = "text/javascript";
+      scr.src = "//t1.daumcdn.net/kas/static/ba.min.js";
+      ins.setAttribute("data-ad-width", width.toString());
+      ins.setAttribute("data-ad-height", height.toString());
+      ins.setAttribute("data-ad-unit", unit.toString());
+      document.querySelector(`.${className}`).appendChild(ins);
+      document.querySelector(`.${className}`).appendChild(scr);
+    }, 100);
+
+    return () => {
+      clearTimeout(timeout);
+    };
   }, []);
 
   return (
