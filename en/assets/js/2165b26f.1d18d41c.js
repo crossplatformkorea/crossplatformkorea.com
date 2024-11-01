ff6ffa57ac03733d12a7b03531fd9da1ee6531b5
hyochan <hyochan@users.noreply.github.com>
2024-11-01 15:15:24 +0000

    deploy: 5597212eb42541d47018a442bd038b48b08882e8

--- en/assets/js/2165b26f.1d18d41c.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkcrossplatformkorea_com=self.webpackChunkcrossplatformkorea_com||[]).push([[3451],{8292:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/en/blog/page/3","page":3,"postsPerPage":10,"totalPages":3,"totalCount":27,"previousPage":"/en/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
---
+++ en/assets/js/22f9ffc3.71783cfb.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkcrossplatformkorea_com=self.webpackChunkcrossplatformkorea_com||[]).push([[2782],{6004:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/en/blog","page":1,"postsPerPage":10,"totalPages":3,"totalCount":28,"nextPage":"/en/blog/page/2","blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
