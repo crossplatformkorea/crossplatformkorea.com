cfb2c49abec5bbc7ec8a890d8493133764619033
Hyo <hyo@hyo.dev>
2025-05-21 23:32:31 +0900

    refactor: profile page

--- src/components/pages/main/index.tsx
+++ src/components/pages/main/index.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { Toaster } from 'sonner';
 import FeatureRequests from '../feature-requests';
-import Posts from './posts-page';
+import Posts from './posts';
 import PostDetails from './post-details';
 import ProfilePage from '../profile';
 
