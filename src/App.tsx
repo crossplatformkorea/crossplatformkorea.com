48701778d8fd6adacad17bb2a02be08fc5505bad
Hyo <hyo@hyo.dev>
2025-05-23 13:57:18 +0900

    feat: title updater

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import { BrowserRouter as Router } from "react-router-dom";
-import AppLayout from "./components/pages";
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppLayout from './components/pages';
+import { useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
 
 export default function App() {
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    document.title = t('common.appName');
+  }, [t]);
+
   return (
     <Router>
       <AppLayout />
